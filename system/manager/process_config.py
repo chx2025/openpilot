9f8db28002380064be9252caba5691bd9524cffa
herizon1054 <spivbaby@gmail.com>
2026-06-26 12:42:55 +0800

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -138,6 +138,7 @@ procs = [
   # dashy
   PythonProcess("serverd", "dragonpilot.dashy.serverd", always_run),
   PythonProcess("dashyd", "dragonpilot.dashy.dashyd", and_(dashy, only_onroad)),
+  PythonProcess("gpsd", "dragonpilot.dashy.gpsd", and_(dashy, only_onroad)),
 ]
 
 managed_processes = {p.name: p for p in procs}
