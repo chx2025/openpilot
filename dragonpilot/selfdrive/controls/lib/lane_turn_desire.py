c49fa26158f45ce6ab5568a77088130a8976a130
herizon1054 <spivbaby@gmail.com>
2026-07-04 13:12:36 +0800

    Update lane_turn_desire.py

--- dragonpilot/selfdrive/controls/lib/lane_turn_desire.py
+++ dragonpilot/selfdrive/controls/lib/lane_turn_desire.py
@@ -1,32 +1,27 @@
 """
 Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
-
-This file is part of sunnypilot and is licensed under the MIT License.
-See the LICENSE.md file in the root directory for more details.
+Adapted for dragonpilot
 """
-from cereal import custom
-
+from cereal import log
 from openpilot.common.constants import CV
 from openpilot.common.params import Params
 
-TurnDirection = custom.ModelDataV2SP.TurnDirection
-
 LANE_CHANGE_SPEED_MIN = 20 * CV.MPH_TO_MS
 
-
 class LaneTurnController:
-  def __init__(self, desire_helper):
-    self.DH = desire_helper
-    self.turn_direction = TurnDirection.none
+  def __init__(self):
+    self.turn_desire = log.Desire.none
     self.params = Params()
-    self.lane_turn_value = float(self.params.get("LaneTurnValue", return_default=True)) * CV.MPH_TO_MS
     self.param_read_counter = 0
-    self.enabled = self.params.get_bool("LaneTurnDesire")
+    self.enabled = False
+    self.lane_turn_value = 10.0 * CV.MPH_TO_MS
+    self.read_params()
 
   def read_params(self):
-    self.enabled = self.params.get_bool("LaneTurnDesire")
-    value = float(self.params.get("LaneTurnValue", return_default=True)) * CV.MPH_TO_MS
-    self.lane_turn_value = min(float(LANE_CHANGE_SPEED_MIN), value)
+    self.enabled = self.params.get_bool("dp_lane_turn_desire")
+    val = self.params.get("dp_lane_turn_value")
+    value = float(val if val is not None else 10.0)
+    self.lane_turn_value = min(float(LANE_CHANGE_SPEED_MIN), value * CV.MPH_TO_MS)
 
   def update_params(self) -> None:
     if self.param_read_counter % 50 == 0:
@@ -35,13 +30,13 @@ class LaneTurnController:
 
   def update_lane_turn(self, blindspot_left: bool, blindspot_right: bool, left_blinker: bool, right_blinker: bool, v_ego: float) -> None:
     if left_blinker and not right_blinker and v_ego < self.lane_turn_value and not blindspot_left:
-      self.turn_direction = TurnDirection.turnLeft
+      self.turn_desire = log.Desire.turnLeft
     elif right_blinker and not left_blinker and v_ego < self.lane_turn_value and not blindspot_right:
-      self.turn_direction = TurnDirection.turnRight
+      self.turn_desire = log.Desire.turnRight
     else:
-      self.turn_direction = TurnDirection.none
+      self.turn_desire = log.Desire.none
 
-  def get_turn_direction(self):
+  def get_turn_desire(self):
     if not self.enabled:
-      return TurnDirection.none
-    return self.turn_direction
+      return log.Desire.none
+    return self.turn_desire
