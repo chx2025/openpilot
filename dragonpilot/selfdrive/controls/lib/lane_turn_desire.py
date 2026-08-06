372f7a6673f3afcdfde90e159b8d3c7abc0fb9f2
herizon1054 <spivbaby@gmail.com>
2026-08-07 00:47:58 +0800

    Update lane_turn_desire.py

--- dragonpilot/selfdrive/controls/lib/lane_turn_desire.py
+++ dragonpilot/selfdrive/controls/lib/lane_turn_desire.py
@@ -6,8 +6,8 @@ from cereal import log
 from openpilot.common.constants import CV
 from openpilot.common.params import Params
 
-# 最大值改為 35 km/h
-LANE_CHANGE_SPEED_MIN = 35 * CV.KPH_TO_MS
+# 最大值改為 30 km/h
+LANE_CHANGE_SPEED_MIN = 30 * CV.KPH_TO_MS
 
 class LaneTurnController:
   def __init__(self):
@@ -30,10 +30,24 @@ class LaneTurnController:
       self.read_params()
     self.param_read_counter += 1
 
-  def update_lane_turn(self, blindspot_left: bool, blindspot_right: bool, left_blinker: bool, right_blinker: bool, v_ego: float) -> None:
-    if left_blinker and not right_blinker and v_ego < self.lane_turn_value and not blindspot_left:
+  def update_lane_turn(self, blindspot_left: bool, blindspot_right: bool, left_blinker: bool, right_blinker: bool, v_ego: float, lane_line_probs: list[float] = None) -> None:
+    """
+    lane_line_probs: 預期傳入前方 5 秒內車道線的機率列表。
+                     (若 Openpilot modelV2 原始輸出為 10 秒 / 33 個點，
+                      請在呼叫端傳入前半段，例如 lane_line_probs[:16])
+    """
+    # 運作條件：前方路線 5 秒內車道線不可以「全部」大於 0.5
+    # (只要有任何一個點的機率低於或等於 0.5，條件即成立，允許運作)
+    lane_condition_met = True
+    if lane_line_probs:
+        lane_condition_met = not all(prob > 0.5 for prob in lane_line_probs)
+
+    # 綜合判斷條件
+    turn_allowed = (v_ego < self.lane_turn_value) and lane_condition_met
+
+    if left_blinker and not right_blinker and turn_allowed and not blindspot_left:
       self.turn_desire = log.Desire.turnLeft
-    elif right_blinker and not left_blinker and v_ego < self.lane_turn_value and not blindspot_right:
+    elif right_blinker and not left_blinker and turn_allowed and not blindspot_right:
       self.turn_desire = log.Desire.turnRight
     else:
       self.turn_desire = log.Desire.none
